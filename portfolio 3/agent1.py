81f737902965d1f4dc92042f34bb7bbe51e848e3
Tizianovdzwet <150351502+Tizianovdzwet@users.noreply.github.com>
2026-06-14 13:51:29 +0200

    na

--- portfolio 3/agent1.py
+++ portfolio 3/agent1.py
@@ -3,7 +3,7 @@ from stable_baselines3 import DQN
 
 class Agent1:
     def __init__(self):
-        self.model = DQN.load("ppo_warlords_final", device="cpu")
+        self.model = DQN.load("dqn_warlords_ram_2000000_steps", device="cpu")
 
     def act(self, observation) -> int:
         obs = np.asarray(observation, dtype=np.float32)
