863dc9066bc172cf77f69f66204cc324e11e0f92
Tizianovdzwet <150351502+Tizianovdzwet@users.noreply.github.com>
2026-06-14 13:59:19 +0200

    .

--- portfolio 3/agent1.py
+++ portfolio 3/agent1.py
@@ -1,9 +1,11 @@
 import numpy as np
 from stable_baselines3 import DQN
+import os
 
 class Agent1:
     def __init__(self):
-        self.model = DQN.load("dqn_warlords_ram_2000000_steps", device="cpu")
+        path = os.path.join(os.path.dirname(__file__), "dqn_warlords_ram_2000000_steps")
+        self.model = DQN.load(path, device="cpu")
 
     def act(self, observation) -> int:
         obs = np.asarray(observation, dtype=np.float32)
